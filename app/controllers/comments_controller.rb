6c2f33519dde9e3653f5490ad28847cfdd7f808f
Hilary (Jitera) <developers@jitera.com>
2024-01-24 05:33:18 +0000

    feat[api]: implement Create Comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,21 @@
 class CommentsController < ApplicationController
 
- 	http_basic_authenticate_with name: "yash", password: "yash", only: :destroy
+  # Removed http_basic_authenticate_with since no authentication is required to create a comment as per the requirement
 
   def create
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    begin
+      @article = Article.find(params[:article_id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "Article not found." }, status: :not_found
+      return
+    end
+
+    @comment = @article.comments.new(comment_params)
+    if @comment.save
+      redirect_to article_path(@article), status: :found
+    else
+      render json: { errors: @comment.errors.full_messages }, status: :bad_request
+    end
   end
  
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  	get 'welcome/index'
+  namespace :v1 do
+    resources :articles, only: [] do
+      resources :comments, only: [:create]
+    end
+  end
 
-  
-	resources :articles do
-	  resources :comments
-	end
- 
-  	root 'welcome#index'
+  resources :articles do
+    resources :comments, only: [:destroy]
+  end
+  # Other routes
 end
